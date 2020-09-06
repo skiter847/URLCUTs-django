7fb701ddf821c1d7d38cbee816ae4fabe91721b9
artem <ivashenco45@gmail.com>
2020-09-07 02:12:27 +0300

    [feature] switch sqlite to postgres

--- urlshort/settings.py
+++ urlshort/settings.py
@@ -67,8 +67,13 @@ WSGI_APPLICATION = 'urlshort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'urlshort',
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
+        'HOST': '127.0.0.1',
+        'PORT': '',
+
     }
 }
 
